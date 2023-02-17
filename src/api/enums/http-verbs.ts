b40db145c462f4761895a3de6084dbf7c3d5b8d1
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2023-02-18 02:30:08 +0800

    reset src/index.ts; add enum: HTTPVerbs

---
+++ src/api/enums/http-verbs.ts
@@ -0,0 +1,54 @@
+/**
+ * HTTP defines a set of request methods to indicate the desired action to be performed for a given resource. Although they can also be nouns, these request methods are sometimes referred to as HTTP verbs.
+ */
+export enum HTTPVerbs {
+  /**
+   * The GET method requests a representation of the specified resource.
+   * Requests using GET should only retrieve data.
+   */
+  GET = 'GET',
+
+  /**
+   * The HEAD method asks for a response identical to a GET request,
+   * but without the response body.
+   */
+  HEAD = 'HEAD',
+
+  /**
+   * The POST method submits an entity to the specified resource,
+   * often causing a change in state or side effects on the server.
+   */
+  POST = 'POST',
+
+  /**
+   * The PUT method replaces all current representations of the
+   * target resource with the request payload.
+   */
+  PUT = 'PUT',
+
+  /**
+   * The DELETE method deletes the specified resource.
+   */
+  DELETE = 'DELETE',
+
+  /**
+   * The CONNECT method establishes a tunnel to the
+   * server identified by the target resource.
+   */
+  CONNECT = 'CONNECT',
+
+  /**
+   * The OPTIONS method describes the communication
+   * options for the target resource.
+   */
+  OPTIONS = 'OPTIONS',
+
+  /**
+   * The TRACE method performs a message loop-back test along the path to the target resource.
+   */
+  TRACE = 'TRACE',
+  /**
+   * The PATCH method applies partial modifications to a resource.
+   */
+  PATCH = 'PATCH',
+}
