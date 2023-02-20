b8af8a1ac23e7117aac04b56e471760f5fd3683e
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2023-02-20 15:47:55 +0800

    add new method in interface RequestHandler

--- src/api/interfaces/request-handler.ts
+++ src/api/interfaces/request-handler.ts
@@ -3,6 +3,7 @@ import { Responder } from '../core/responder';
 
 export interface RequestHandler {
   canHandle(req: Requester): boolean;
+  matchPath(path: String | RegExp): boolean;
   handle(req: Requester, res: Responder, err?: any): Promise<void>;
 }
 
