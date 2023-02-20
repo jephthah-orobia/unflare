8e264ff6ff838c64494d0d75d36c7e80e057fc3a
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2023-02-20 15:56:21 +0800

    modify return value of matchPath

--- src/api/interfaces/request-handler.ts
+++ src/api/interfaces/request-handler.ts
@@ -1,9 +1,10 @@
 import { Requester } from '../core/requester';
 import { Responder } from '../core/responder';
+import { Route } from '../core/route';
 
 export interface RequestHandler {
   canHandle(req: Requester): boolean;
-  matchPath(path: String | RegExp): boolean;
+  matchPath(path: String | RegExp): { isMatch: boolean; route?: Route };
   handle(req: Requester, res: Responder, err?: any): Promise<void>;
 }
 
