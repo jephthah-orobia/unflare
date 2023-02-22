344403307e6a6b82983eb08384590a5b2fa6614b
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2023-02-22 22:37:58 +0800

    router tests passed.

--- src/api/core/router.test.ts
+++ src/api/core/router.test.ts
@@ -1,5 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import { HTTPVerbs } from '../enums/http-verbs';
+import { Requester } from './requester';
+import { Responder } from './responder';
 import { Router } from './router';
 
 describe('Assumptions', () => {
@@ -13,12 +15,10 @@ describe('Assumptions', () => {
   });
 });
 
-describe('Router', () => {
+describe('Router Constructor', () => {
   it('Property test', () => {
     const router = new Router();
-    return;
     expect(router).toHaveProperty('use');
-    expect(router).toHaveProperty('any');
     expect(router).toHaveProperty('get');
     expect(router).toHaveProperty('head');
     expect(router).toHaveProperty('post');
@@ -27,11 +27,83 @@ describe('Router', () => {
     expect(router).toHaveProperty('connect');
     expect(router).toHaveProperty('options');
     expect(router).toHaveProperty('patch');
+    expect(router).toHaveProperty('all');
+  });
+});
+
+describe('Router::canHandle()', () => {
+  it('should return false if the request method is not added to router', () => {
+    const route1 = new Router();
+    const req1 = new Requester(
+      new Request('https://example.com/api/users', { method: 'GET' })
+    );
+    route1.post('/api/users', (req: Requester, res: Responder) => {
+      res.send('Hello');
+    });
+    expect(route1.canHandle(req1)).toStrictEqual(false);
   });
 
-  it('use Test', () => {
-    const router1 = new Router();
+  it('should return true if the request method is added to router', () => {
+    const route1 = new Router();
+    const req1 = new Requester(
+      new Request('https://example.com/api/users', { method: 'GET' })
+    );
+    route1
+      .get('/api/users', (req: Requester, res: Responder) => {
+        res.send('Hello');
+      })
+      .post('/api/users', (req: Requester, res: Responder) => {
+        res.send('You ask to post?');
+      });
+    expect(route1.canHandle(req1)).toStrictEqual(true);
+  });
 
-    const router2 = new Router();
+  it('should return true if the router has a handler with method ALL', () => {
+    const route1 = new Router();
+    const req1 = new Requester(
+      new Request('https://example.com/api/users', { method: 'GET' })
+    );
+    route1.all('/api/users', (req: Requester, res: Responder) => {
+      res.send('Method for All');
+    });
+    expect(route1.canHandle(req1)).toStrictEqual(true);
+  });
+
+  it('should return true for request whose path matches the router with params', () => {
+    const route1 = new Router();
+    const req1 = new Requester(
+      new Request('https://example.com/api/users/1234/email/test@test.test', {
+        method: 'GET',
+      })
+    );
+    route1.get(
+      '/api/users/:id/email/:email',
+      (req: Requester, res: Responder) => {
+        res.send('Method for All');
+      }
+    );
+    expect(route1.canHandle(req1)).toStrictEqual(true);
+  });
+});
+
+describe('Router::handle()', () => {
+  it('should set the params property of the requester and set the isDone property of the responder given valid handlers', () => {
+    const route1 = new Router();
+    const req1 = new Requester(
+      new Request('https://example.com/api/users/1234/email/test@test.test', {
+        method: 'GET',
+      })
+    );
+    route1.get(
+      '/api/users/:id/email/:email',
+      (req: Requester, res: Responder) => {
+        res.send('Method for All');
+      }
+    );
+    expect(route1.canHandle(req1)).toStrictEqual(true);
+    const res = new Responder('example.com');
+    expect(() => route1.handle(req1, res)).not.toThrowError();
+    expect(req1.params).toStrictEqual({ id: '1234', email: 'test@test.test' });
+    expect(res.isDone).toStrictEqual(true);
   });
 });
--- src/api/core/router.ts
+++ src/api/core/router.ts
@@ -19,22 +19,61 @@ import { Responder } from './responder';
 import { Route } from './route';
 
 import { isPromise } from '../utils/fn/is-promise';
+import { flattenArray } from '../utils/fn/flatten-array';
+import { HTTPVerbs, stringToHTTPVerbs } from '../enums/http-verbs';
+import {
+  AsyncRouteHandler,
+  isAsyncRouteHandler,
+  isRouteHandler,
+  RouteHandler,
+} from '../interfaces/route-handler';
+
+type Handler =
+  | Middleware
+  | AsyncMiddleware
+  | ErrorHandler
+  | AsyncErrorHandler
+  | RequestHandler;
+
+const isHandler = (obj: any): obj is Handler => {
+  return (
+    isMiddleware(obj) ||
+    isAsyncMiddleware(obj) ||
+    isErrorHandler(obj) ||
+    isAsyncErrorHandler(obj) ||
+    isRequestHandler(obj)
+  );
+};
+
+type RouteHandlers =
+  | RouteHandler
+  | AsyncRouteHandler
+  | Middleware
+  | AsyncMiddleware
+  | ErrorHandler
+  | AsyncErrorHandler;
+
+const isRouteHandlers = (obj: any): obj is Handler => {
+  return (
+    isRouteHandler(obj) ||
+    isAsyncRouteHandler(obj) ||
+    isMiddleware(obj) ||
+    isAsyncMiddleware(obj) ||
+    isErrorHandler(obj) ||
+    isAsyncErrorHandler(obj)
+  );
+};
 
 export class Router implements RequestHandler {
   #index: number = -1;
-  #handlers: (
-    | Middleware
-    | AsyncMiddleware
-    | ErrorHandler
-    | AsyncErrorHandler
-    | RequestHandler
-  )[] = [];
+  #handlers: Handler[] = [];
   #req: Requester | undefined;
   #res: Responder | undefined;
 
   #next(err?: any): void {
     if (!this.#req || !this.#res)
       throw new Error('No request found or responder object is not created.');
+    if (this.#res.isDone) return;
     if (this.#handlers.length > this.#index + 1) {
       let returnValue: void | Promise<void> | boolean;
       const handler = this.#handlers[++this.#index];
@@ -46,25 +85,131 @@ export class Router implements RequestHandler {
         returnValue = handler(err, this.#req, this.#res, this.#next);
       else if (err && isAsyncErrorHandler(handler))
         returnValue = handler(err, this.#req, this.#res, this.#next);
-      else if (!err && handler instanceof Route && handler.canHandle(this.#req))
+      else if (
+        !err &&
+        isRequestHandler(handler) &&
+        handler.canHandle(this.#req)
+      )
         returnValue = handler.handle(this.#req, this.#res);
-      else if (handler instanceof Route && handler.canHandle(this.#req))
+      else if (
+        handler &&
+        isRequestHandler(handler) &&
+        handler.canHandle(this.#req)
+      )
         returnValue = handler.handle(this.#req, this.#res, err);
       if (isPromise<void>(returnValue)) returnValue.then(() => {});
     }
   }
 
+  routeOfPath(path: string | RegExp): Route | null {
+    for (const handler of this.#handlers)
+      if (isRequestHandler(handler)) {
+        const route = handler.routeOfPath(path);
+        if (route) return route;
+      }
+    return null;
+  }
+
   canHandle(req: Requester): boolean {
-    for (const r of this.#handlers)
-      if (isRequestHandler(r) && r.canHandle(req)) return true;
-    return false;
+    return this.#handlers.some(
+      (handler) => isRequestHandler(handler) && handler.canHandle(req)
+    );
   }
+
   async handle(req: Requester, res: Responder, err?: any): Promise<void> {
-    this.#req = req;
-    this.#res = res;
-    this.#index = -1;
-    if (!err) this.#next();
-    else this.#next(err);
-    this.#index = -1;
+    if (this.canHandle(req)) {
+      this.#req = req;
+      this.#res = res;
+      this.#index = -1;
+      if (!err) this.#next();
+      else this.#next(err);
+      this.#index = -1;
+    }
   }
+
+  route(pathOrPattern: string | RegExp): Route {
+    const route = this.routeOfPath(pathOrPattern);
+    if (!route) {
+      const newRoute = new Route(pathOrPattern);
+      this.#handlers.push(newRoute);
+      return newRoute;
+    }
+    return route;
+  }
+
+  use(...args: Handler[]): Router {
+    const flattenArgs = flattenArray(...args);
+    for (const e of flattenArgs) {
+      if (isHandler(e)) this.#handlers.push(e);
+    }
+    return this;
+  }
+
+  _handle_methods(
+    method: HTTPVerbs,
+    path: string | RegExp,
+    ...args: (RouteHandlers | RouteHandlers[])[]
+  ): Router {
+    const route: Route = this.route(path);
+    route._handle_methods(method, ...args);
+    return this;
+  }
+}
+
+export declare interface Router {
+  head(
+    path: string | RegExp,
+    ...args: (RouteHandlers | RouteHandlers[])[]
+  ): Router;
+  get(
+    path: string | RegExp,
+    ...args: (RouteHandlers | RouteHandlers[])[]
+  ): Router;
+  post(
+    path: string | RegExp,
+    ...args: (RouteHandlers | RouteHandlers[])[]
+  ): Router;
+  put(
+    path: string | RegExp,
+    ...args: (RouteHandlers | RouteHandlers[])[]
+  ): Router;
+  patch(
+    path: string | RegExp,
+    ...args: (RouteHandlers | RouteHandlers[])[]
+  ): Router;
+  delete(
+    path: string | RegExp,
+    ...args: (RouteHandlers | RouteHandlers[])[]
+  ): Router;
+  connect(
+    path: string | RegExp,
+    ...args: (RouteHandlers | RouteHandlers[])[]
+  ): Router;
+  trace(
+    path: string | RegExp,
+    ...args: (RouteHandlers | RouteHandlers[])[]
+  ): Router;
+  options(
+    path: string | RegExp,
+    ...args: (RouteHandlers | RouteHandlers[])[]
+  ): Router;
+  all(
+    path: string | RegExp,
+    ...args: (RouteHandlers | RouteHandlers[])[]
+  ): Router;
+
+  [index: string]: any;
+}
+
+for (const verb in HTTPVerbs) {
+  Router.prototype[verb.toLowerCase()] = function (
+    pathOrPattern: string | RegExp,
+    ...args: (RouteHandlers | RouteHandlers[])[]
+  ): Router {
+    return this._handle_methods(
+      stringToHTTPVerbs(verb),
+      pathOrPattern,
+      ...args
+    );
+  };
 }
