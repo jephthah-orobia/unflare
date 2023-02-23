15f3dbe11b9f0f855df4d7db660b4273c590cd4c
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2023-02-24 05:27:19 +0800

    export Unflare class as default

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { Unflare } from './api/core/unflare';
+
 export * from './api/core/unflare';
 export * from './api/core/requester';
 export * from './api/core/responder';
@@ -11,3 +13,5 @@ export * from './api/interfaces/error-handler';
 export * from './api/interfaces/middleware';
 export * from './api/interfaces/route-handler';
 export * from './api/interfaces/request-handler';
+
+export default Unflare;
