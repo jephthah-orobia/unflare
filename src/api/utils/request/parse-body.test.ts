e60ffafabd78df971d02d84b1bfde1ea99e7e59e
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2023-02-25 01:39:30 +0800

    new util: parseBody of request

---
+++ src/api/utils/request/parse-body.test.ts
@@ -0,0 +1,90 @@
+import { describe, expect, it } from 'vitest';
+import { parseBody } from './parse-body';
+import mime from 'mime-types';
+import queryString from 'node:querystring';
+
+describe('parseBody', () => {
+  it('should return null if stream is empty', async () => {
+    const req = new Request('https://example.com/users', {
+      method: 'GET',
+    });
+    const result = await parseBody(req);
+    expect(result).not.toBe({});
+    expect(result).toBeNull();
+  });
+
+  it('should return a string if the stream is a text/*', async () => {
+    const req = new Request('https://example.com/users', {
+      method: 'POST',
+      body: 'abc123',
+    });
+
+    const encoder = new TextEncoder();
+    req.headers.set('Content-Type', 'text/plain');
+    req.headers.set(
+      'Content-Length',
+      encoder.encode('abc123').length.toString()
+    );
+    const body = await parseBody(req);
+    expect(typeof body === 'string').toStrictEqual(true);
+    expect(body).toStrictEqual('abc123');
+  });
+
+  it('should return an object if the stream is an application/json', async () => {
+    const obj = { email: 'test@test.test', password: 'Abc123' };
+
+    const req = new Request('https://example.com/users', {
+      method: 'POST',
+      body: JSON.stringify(obj),
+    });
+
+    const encoder = new TextEncoder();
+    req.headers.set('Content-Type', mime.contentType('json') as string);
+    req.headers.set(
+      'Content-Length',
+      encoder.encode(JSON.stringify(obj)).length.toString()
+    );
+    console.log(encoder.encode(JSON.stringify(obj)).length.toString());
+
+    expect(await parseBody(req)).toStrictEqual(obj);
+  });
+
+  it('should return an object if the stream is an application/x-www-form-urlencoded', async () => {
+    const obj = { email: 'test@test.test', password: 'Abc123' };
+
+    const req = new Request('https://example.com/users', {
+      method: 'POST',
+      body: queryString.encode(obj),
+    });
+
+    const encoder = new TextEncoder();
+    req.headers.set('Content-Type', 'application/x-www-form-urlencoded');
+    req.headers.set(
+      'Content-Length',
+      encoder.encode(queryString.encode(obj)).length.toString()
+    );
+    const body = await parseBody(req);
+    expect(body).toHaveProperty('email');
+    expect(body.email).toStrictEqual(obj.email);
+    expect(body).toHaveProperty('password');
+    expect(body.password).toStrictEqual(obj.password);
+  });
+
+  it('should return a Uint8array object if the stream is of other types', async () => {
+    const obj = { email: 'test@test.test', password: 'Abc123' };
+    const encoder = new TextEncoder();
+    const data = encoder.encode(JSON.stringify(obj));
+    const req = new Request('https://example.com/users', {
+      method: 'POST',
+      body: data,
+    });
+
+    req.headers.set('Content-Length', data.length.toString());
+    const body = await parseBody(req);
+    expect(typeof body).toStrictEqual('object');
+    expect(body instanceof Uint8Array).toStrictEqual(true);
+    expect(body[3]).toStrictEqual(encoder.encode(JSON.stringify(obj))[3]);
+    expect(body[5]).toStrictEqual(encoder.encode(JSON.stringify(obj))[5]);
+    expect(body[7]).toStrictEqual(encoder.encode(JSON.stringify(obj))[7]);
+  });
+});
---
+++ src/api/utils/request/parse-body.ts
@@ -0,0 +1,47 @@
+import mime from 'mime-types';
+import { typeIs } from './type-is';
+import queryString from 'node:querystring';
+
+export async function parseBody(req: Request): Promise<any> {
+  if (!req.body) return null;
+
+  const lengthStr =
+    req.headers.get('Content-Length') || req.headers.get('content-length');
+
+  if (!lengthStr) {
+    console.error('Content-Length of header is invalid!');
+    return null;
+  }
+  const length = parseInt(lengthStr);
+  console.log('content-length:', length);
+  if (isNaN(length) || length < 0) {
+    console.error('Content-Length of header is invalid');
+    return null;
+  }
+
+  const reader = req.body.getReader();
+  const bodyUInt8: Uint8Array = new Uint8Array(length);
+
+  let index = -1;
+
+  const appendValueToBodyUInt8 = ({
+    done,
+    value,
+  }: ReadableStreamReadResult): void | PromiseLike<void> => {
+    if (done) return;
+    else for (const val of value) bodyUInt8[++index] = val;
+    return reader.read().then(appendValueToBodyUInt8);
+  };
+  await reader.read().then(appendValueToBodyUInt8);
+
+  const textdecoder = new TextDecoder();
+  if (typeIs(req, /^text\//)) return textdecoder.decode(bodyUInt8);
+
+  if (typeIs(req, /application\/json/i))
+    return JSON.parse(textdecoder.decode(bodyUInt8));
+
+  if (typeIs(req, /urlencoded/))
+    return queryString.decode(textdecoder.decode(bodyUInt8));
+
+  return bodyUInt8;
+}
