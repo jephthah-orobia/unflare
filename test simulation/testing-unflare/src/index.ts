a0607d855e8ecdc1dd5d9e032fa2fec063ceaa69
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2023-02-26 00:12:21 +0800

    Manual testing in test-simulation pass!

--- test simulation/testing-unflare/src/index.ts
+++ test simulation/testing-unflare/src/index.ts
@@ -26,15 +26,53 @@ import { Unflare, Requester, Responder } from '../../../';
 export const app = new Unflare();
 
 app.get('/', function (req: Requester, res: Responder) {
-  res.send('Hello World!');
+  res.headers.set('Content-Type', 'text/html');
+  if (!req.cookies.user) {
+    return res.send(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title> You are not logged in! </title>
+        </head>
+        <body>
+          <h1> Sign In</h1>
+          <form action="/users" method="POST">
+            <input type="text" name="name" />
+            <input type="submit" />
+          </form>
+        </body>
+      </html>
+    `);
+  } else {
+    const user = JSON.parse(req.cookies.user);
+    return res.send(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title> You are logged in! </title>
+        </head>
+        <body>
+          <h1> Welcome ${user.name}</h1>
+          <form action="/signout" method="GET">
+            <input type="submit" value="Sign Out" />
+          </form>
+        </body>
+      </html>
+    `);
+  }
 });
 
-app.get('/users', function (req: Requester, res: Responder) {
-  res.send('Users page');
+app.post('/users', function (req: Requester, res: Responder) {
+  if (!req.body.name) return res.status(401).send('name');
+  res.cookie('user', JSON.stringify({ name: req.body.name, id: 12451231521 }));
+  res.status(302, 'Redirecting').headers.set('Location', '/');
+  res.send();
 });
 
-app.get('/users/:id', (req: Requester, res: Responder) => {
-  res.send(`User page, you were trying to view: ${req.params.id}`);
+app.get('/signout', (req: Requester, res: Responder) => {
+  res.cookie('user', '');
+  res.status(302, 'Reedirecting').headers.set('Location', '/');
+  res.send();
 });
 
 export default app;
