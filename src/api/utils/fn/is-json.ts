b90423150b8f73d0fc2d33925e7b366a823c8b99
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2023-02-25 19:53:12 +0800

    add some comment to isJson()

--- src/api/utils/fn/is-json.ts
+++ src/api/utils/fn/is-json.ts
@@ -1,3 +1,8 @@
+/**
+ * checks wether a string is a JSON string or not.
+ * @param string to inspect if it is a JSON string.
+ * @returns [boolean] true or false
+ */
 export function isJson(str: string): boolean {
   try {
     const obj = JSON.parse(str);
