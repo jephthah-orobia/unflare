5326b2fe0df73636fd29a7f9d37774c678f62714
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2023-03-08 13:17:06 +0800

    fix #8: change prop to method: notFoundResponse => createNotFoundResponse

--- src/api/core/unflare.ts
+++ src/api/core/unflare.ts
@@ -30,14 +30,23 @@ export class Unflare extends Router {
   }
 
   /**
-   * - Set a new `Response` to this to customize 'Not Found Page';
+   * - creates a new `Response` to this to customize 'Not Found Page';
    * - ie: Set to a response with status code `302` to redirect to a static page.
    * - For more information in constructing a response, check worker's docs on [Response](https://developers.cloudflare.com/workers/runtime-apis/response/)
    */
-  notFoundResponse: Response = new Response('Page Not Found!', {
-    status: 404,
-    statusText: 'Page Not Found!',
-  });
+  createNotFoundResponse = (): Response =>
+    new Response('Page Not Found!', {
+      status: 404,
+      statusText: 'Page Not Found!',
+    });
+
+  /**
+   * Sets the generator function for the `Not Found` page; Make sure to set status to 404
+   * @param creator a function that generates a response that will be use for "NOT FOUND PAGE".
+   */
+  setNotFound(creator: () => Response) {
+    this.createNotFoundResponse = creator;
+  }
 
   static Router(): Router {
     return new Router();
@@ -59,7 +68,7 @@ export class Unflare extends Router {
       err = e;
     }
     if (reser.isDone) return reser.response!;
-    else if (!err) return this.notFoundResponse;
+    else if (!err) return this.createNotFoundResponse();
     else
       return new Response(err, {
         status: 500,
