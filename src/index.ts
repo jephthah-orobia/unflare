45e4e8099604509c5107f24f447c8911ef19b7d6
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2023-03-09 02:00:57 +0800

    expose Route and Router via index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,11 @@ export * from './api/core/unflare';
 
 export * from './api/core/responder';
 export * from './api/core/requester';
+export * from './api/core/route';
+export * from './api/core/router';
 
 export * from './api/interfaces/error-handler';
 export * from './api/interfaces/middleware';
 export * from './api/interfaces/route-handler';
 
 export default Unflare;
-
-//some changes here
-// moree chgaes
